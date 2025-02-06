15f2adabd6c30a6059b4fab9e8526e369f57fd98
Mayendra20coding <bandara.mayendra@gmail.com>
2025-02-06 20:31:45 +1300

    divisblenumber

---
+++ divsbiblenumber.py
@@ -0,0 +1,8 @@
+print('enter a numerator number')
+numerator=int(input('enter a numerator'))
+print('enter a denominator number')
+denominator=int(input('enter a denominator'))
+if numerator%denominator==0:
+ print('number is divisble')
+else:
+ print('number is not dividble')
